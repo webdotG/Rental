5d4e00d329a2935da5351b4dbbe7b7b2602635ac
Grant Kirill <webdotg.inbox@gmail.com>
2023-10-16 19:06:21 +0300

    fix type bug dispatch

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { useDispatch } from 'react-redux'
 import filter from './slices/filterSlice'
 import cart from './slices/cartSlice'
 import items from './slices/itemsSlice'
@@ -13,4 +14,4 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
-export const useAppDispatch = () =>  useDispatch<AppDispatch>() 
+export const useAppDispatch: () => AppDispatch = useDispatch
