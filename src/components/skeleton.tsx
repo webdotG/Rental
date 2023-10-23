45b52559468eede781acbf53424f54d1d1a211a6
Grant Kirill <webdotg.inbox@gmail.com>
2023-10-23 13:53:45 +0300

    style base skeleton add

--- src/components/skeleton.tsx
+++ src/components/skeleton.tsx
@@ -1,21 +1,15 @@
 import ContentLoader from "react-content-loader"
 
 const Skeleton = () => (  //props
-  <ContentLoader
-  className="pizza-block" 
+<ContentLoader 
     speed={2}
     width={280}
-    height={465}
-    viewBox="0 0 280 465"
+    height={516}
+    viewBox="0 0 280 516"
     backgroundColor="#f3f3f3"
     foregroundColor="#ecebeb"
-    // {...props}
   >
-    <circle cx="135" cy="134" r="125" /> 
-    <rect x="0" y="279" rx="10" ry="10" width="280" height="23" /> 
-    <rect x="0" y="326" rx="10" ry="10" width="280" height="88" /> 
-    <rect x="0" y="411" rx="15" ry="15" width="90" height="30" /> 
-    <rect x="149" y="407" rx="15" ry="15" width="120" height="40" />
+    <rect x="48" y="0" rx="0" ry="0" width="280" height="516" />
   </ContentLoader>
 )
 
