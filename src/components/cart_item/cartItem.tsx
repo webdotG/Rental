a6b1cfe21136a5e5a9b20506753f7fdbf548c805
Grant Kirill <webdotg.inbox@gmail.com>
2023-10-23 21:20:12 +0300

    cart page style add

--- src/components/cart_item/cartItem.tsx
+++ src/components/cart_item/cartItem.tsx
@@ -43,8 +43,8 @@ function CartItem({ id, title, price, count, image, type, sizes }: typeCartItemP
           <button disabled={count === 1} onClick={onClickMinus}
             className={style.cart__item_count_minus}>
             <svg
-              width="14"
-              height="14"
+              width="18"
+              height="18"
               viewBox="0 0 10 10"
               fill="black"
               xmlns="http://www.w3.org/2000/svg"
@@ -53,7 +53,6 @@ function CartItem({ id, title, price, count, image, type, sizes }: typeCartItemP
                 d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
               /> */}
               <path
-                width="20"
                 d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
               />
             </svg>
@@ -61,8 +60,8 @@ function CartItem({ id, title, price, count, image, type, sizes }: typeCartItemP
           <p>{count}</p>
           <button onClick={onClickPlus} className={style.cart__item_count_plus}>
             <svg
-              width="14"
-              height="14"
+              width="18"
+              height="18"
               viewBox="0 0 10 10"
               fill="black"
               xmlns="http://www.w3.org/2000/svg"
--- src/pages/cartPage.tsx
+++ src/pages/cartPage.tsx
@@ -22,40 +22,9 @@ function Cart() {
     return <CartEmpty />
   }
   return (
-    <div >
-      <div >
         <div className={style.cart}>
           <div className={style.cart__top}>
-            <h2 className={style.content__title}>
-              <svg
-                width="18"
-                height="18"
-                viewBox="0 0 18 18"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M6.33333 16.3333C7.06971 16.3333 7.66667 15.7364 7.66667 15C7.66667 14.2636 7.06971 13.6667 6.33333 13.6667C5.59695 13.6667 5 14.2636 5 15C5 15.7364 5.59695 16.3333 6.33333 16.3333Z"
-                  stroke="white"
-                  strokeWidth="1.8"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M14.3333 16.3333C15.0697 16.3333 15.6667 15.7364 15.6667 15C15.6667 14.2636 15.0697 13.6667 14.3333 13.6667C13.597 13.6667 13 14.2636 13 15C13 15.7364 13.597 16.3333 14.3333 16.3333Z"
-                  stroke="white"
-                  strokeWidth="1.8"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M4.78002 4.99999H16.3334L15.2134 10.5933C15.1524 10.9003 14.9854 11.176 14.7417 11.3722C14.4979 11.5684 14.1929 11.6727 13.88 11.6667H6.83335C6.50781 11.6694 6.1925 11.553 5.94689 11.3393C5.70128 11.1256 5.54233 10.8295 5.50002 10.5067L4.48669 2.82666C4.44466 2.50615 4.28764 2.21182 4.04482 1.99844C3.80201 1.78505 3.48994 1.66715 3.16669 1.66666H1.66669"
-                  stroke="white"
-                  strokeWidth="1.8"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
+            <h2 className={style.cart_content__title}>
               Корзина
             </h2>
             <button onClick={onClickClear} className={style.cart__clear}>
@@ -68,34 +37,33 @@ function Cart() {
                 >
                   <path
                     d="M2.5 5H4.16667H17.5"
-                    stroke="#B6B6B6"
+                    stroke="white"
                     strokeWidth="1.2"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                   />
                   <path
                     d="M6.66663 5.00001V3.33334C6.66663 2.89131 6.84222 2.46739 7.15478 2.15483C7.46734 1.84227 7.89127 1.66667 8.33329 1.66667H11.6666C12.1087 1.66667 12.5326 1.84227 12.8451 2.15483C13.1577 2.46739 13.3333 2.89131 13.3333 3.33334V5.00001M15.8333 5.00001V16.6667C15.8333 17.1087 15.6577 17.5326 15.3451 17.8452C15.0326 18.1577 14.6087 18.3333 14.1666 18.3333H5.83329C5.39127 18.3333 4.96734 18.1577 4.65478 17.8452C4.34222 17.5326 4.16663 17.1087 4.16663 16.6667V5.00001H15.8333Z"
-                    stroke="#B6B6B6"
+                    stroke="white"
                     strokeWidth="1.2"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                   />
                   <path
                     d="M8.33337 9.16667V14.1667"
-                    stroke="#B6B6B6"
+                    stroke="white"
                     strokeWidth="1.2"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                   />
                   <path
                     d="M11.6666 9.16667V14.1667"
-                    stroke="#B6B6B6"
+                    stroke="white"
                     strokeWidth="1.2"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                   />
                 </svg>
-
                 <span>Очистить корзину</span>
               </button>
           </div>
@@ -123,7 +91,7 @@ function Cart() {
               <Link className={style.cart__bottom_link_main}
                 to="/Rental"
               >
-                <svg
+                {/* <svg
                   width="8"
                   height="14"
                   viewBox="0 0 8 14"
@@ -137,18 +105,16 @@ function Cart() {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                   />
-                </svg>
+                </svg> */}
                 На главную
               </Link>
               <button className={style.pay_btn}>
-                <span>Оплатить сейчас</span>
+                <span>Оплатить</span>
               </button>
 
             </div>
           </div>
         </div>
-      </div>
-    </div>
   );
 }
 
