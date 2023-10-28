06653eab5ce7620545e2ba67c845e58d2cdca2ed
Grant Kirill <webdotg.inbox@gmail.com>
2023-10-28 18:48:38 +0300

    test new  style register page

--- src/pages/pageRegister.tsx
+++ src/pages/pageRegister.tsx
@@ -1,16 +1,16 @@
 import { Link } from 'react-router-dom'
-import  SingUp  from '../components/singUp/singUp'
+import SingUp from '../components/singUp/singUp'
 import style from '../scss/pages/registerPage.module.scss'
 
-function RegisterPage () {
+function RegisterPage() {
 
-  return(
-    <>
-    <h1 className={style.title}>Зарегестрироваться</h1>
-    <SingUp />
-    <p className={style.text}>или</p>
-    <Link className={style.link_login} to='/Rental/login'>войти</Link>
-    </>
+  return (
+    <div className={style.register_page_wrapper}>
+      <h1 className={style.title}>Регистрация</h1>
+      <SingUp />
+      <p className={style.text}>или</p>
+      <Link className={style.link_login} to='/Rental/login'>войти</Link>
+    </div>
   )
 }
 
