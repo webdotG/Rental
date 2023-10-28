fa46b573158d02cba7bcbc79210ddba112b534e9
Grant Kirill <webdotg.inbox@gmail.com>
2023-10-28 18:43:05 +0300

    test new  style homePage

--- src/pages/pageLogin.tsx
+++ src/pages/pageLogin.tsx
@@ -5,12 +5,12 @@ import style from '../scss/pages/loginPage.module.scss'
 function LoginPage() {
 
   return (
-    <>
+    <div className={style.page_login_wrapper}>
       <h1 className={style.title}>Войти</h1>
       <Login />
       <p className={style.text}>или</p>
       <Link className={style.link_register} to='/Rental/register'>зарегистрироваться</Link>
-    </>
+    </div>
   )
 }
 
