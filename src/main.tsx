b855d97873602e5ce2d0eec51d263c589b3b4a85
Grant Kirill <webdotg.inbox@gmail.com>
2023-11-15 23:08:27 +0300

    test fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { HashRouter } from "react-router-dom";  //BrowserRouter,
+import {BrowserRouter} from "react-router-dom";  // HashRouter 
 import { store } from './shared-kernel/store'
 import { Provider } from 'react-redux'
 import './FIREBASE.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
          <App/>
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
 )
