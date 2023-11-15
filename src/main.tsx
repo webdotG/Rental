aef559f7b19d9458d70348daf81e1efc1e3428bc
Grant Kirill <webdotg.inbox@gmail.com>
2023-11-15 22:23:46 +0300

    sale HASHROUTER GIT BUG FIX TEST

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";  //BrowserRouter,
 import { store } from './shared-kernel/store'
 import { Provider } from 'react-redux'
 import './FIREBASE.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
          <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
 )
